5dd42f0dbaf8c98b71c44bda4d69fe524cdf63c8
jayapatel1623 <120316165+jayapatel1623@users.noreply.github.com>
2023-02-12 14:32:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,46 @@
-import React from 'react'
+
+import React, { useEffect, useState } from 'react'
 import '../styles/App.css';
 import { Loader } from './Loader';
 import { PhotoFrame } from './PhotoFrame';
-const fetchPosts = async (id) =>{
-  let url = `https://jsonplaceholder.typicode.com/photos/${id}`;
+
+const fetchPosts = async (id) => {
+    let url = `https://jsonplaceholder.typicode.com/photos/${id}`;
   return fetch(url);
 }
-const App = () => {
-  const[dataaa,setData]=useState([])
-  const[id,setId]=useState(null)
-  const handleChange=(e)=>{
-    setId(e.target.value)
-  
-}
-const loadData=async ()=>{
-  fetchPosts(id)
-  .then((res)=>res.json())
-  .then((jsonData)=>{
-    setData(jsonData);
-  })
-}
-useEffect(()=>{
-  loadData()
-},[id])
-  return(
-    <div>
-    <span>Id number</span>
-    <input type="number"onChange={handleChange}/>
-{data==null
 
-export default App;
+const App = () => {
+    const[data,setData]=useState([])
+    const[id,setId]=useState(null)
+    const handleChange=(e)=>{
+        setId(e.target.value)
+    }
+    
+    const loadData = async () => {
+        fetchPosts(id)
+          .then((res) => res.json())
+          .then((jsonData) => {
+            setData(jsonData);
+          })
+      }
+    
+      useEffect(() => {
+        loadData();
+      }, []);
+    
+      useEffect(() => {
+        setData(null)
+        loadData()
+      }, [id])
+
+    return(
+        <div>
+            <span>Id number</span>
+            <input type="number" onChange={handleChange} />
+            {data == null ? ( <Loader/> ) : <PhotoFrame url={data.url} title={data.title}/>}
+        </div>
+    )
+}
+
+
+export default App
